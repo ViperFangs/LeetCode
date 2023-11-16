837a65508436af35df9b9a9b1fca1405027e3a87
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-11-16 00:34:31 -0700

    Add time complexity comment in 217_Contains_Duplicates/solution.py

--- 217_Contains_Duplicates/solution.py
+++ 217_Contains_Duplicates/solution.py
@@ -3,6 +3,7 @@ Author: Aarya
 Description: Given an integer array nums, 
     return true if any value appears at least twice in the array,
     and return false if every element is distinct.
+Time Complexity: O(n)
 """
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
