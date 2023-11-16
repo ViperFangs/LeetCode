68689d5cda9367b325b9f9e3ca32ad13ca8bc8e5
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-11-15 22:58:39 -0700

    Add comments in 217_Contains_Duplicates/Solution.py

--- 217_Contains_Duplicates/solution.py
+++ 217_Contains_Duplicates/solution.py
@@ -1,9 +1,21 @@
+""" 
+Author: Aarya
+Description: Given an integer array nums, 
+    return true if any value appears at least twice in the array,
+    and return false if every element is distinct.
+"""
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        """
+        The time complexity for a set is O(1) for the following operations: 
+        lookup/insert/delete
+        """
         hashset = set()
         for num in nums:
+            # if the number already exists in the set then return true
             if num in hashset:
                 return True
+            # if the number doesn't exist in the set then add it to the set
             else:
                 hashset.add(num)
         return False
