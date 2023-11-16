801d5d4378b855c9c89ca8a8438dc0e837e36633
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-11-16 00:26:59 -0700

    Implement Valid Anagram solution

---
+++ 242_valid_anagram/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
+
+        for c in countS:
+            if countS[c] != countT.get(c):
+                return False
+        
+        return True
