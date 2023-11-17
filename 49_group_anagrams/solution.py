4f29ec1e44eb666f10b02ad9b622af0c896759ea
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-11-16 23:54:07 -0700

    Write the correct time complexity for Group Anagrams

--- 49_group_anagrams/solution.py
+++ 49_group_anagrams/solution.py
@@ -3,7 +3,7 @@ Author: Aarya
 Description: Given an array of strings strs, group the anagrams together. You can return the answer in any order.
     An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
     typically using all the original letters exactly once.
-Time Complexity: O(m + n) where m is the number of strings and n is the length of each string
+Time Complexity: O(m * n) where m is the number of strings and n is the average length of each string
 """
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
