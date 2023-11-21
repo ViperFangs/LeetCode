fa3315a0c0552f1f83a568da25f23c54bcffd651
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-11-21 02:40:08 -0700

    Add author, description and Time Complextity as a comment to solution.py for Robot Bounded by Circle

--- 1041_robot_bounded_by_circle/solution.py
+++ 1041_robot_bounded_by_circle/solution.py
@@ -1,3 +1,20 @@
+""" 
+Author: Aarya
+Description: On an infinite plane, a robot initially stands at (0, 0) and faces north. Note that:
+    The north direction is the positive direction of the y-axis.
+    The south direction is the negative direction of the y-axis.
+    The east direction is the positive direction of the x-axis.
+    The west direction is the negative direction of the x-axis.
+    
+    The robot can receive one of three instructions:
+        "G": go straight 1 unit.
+        "L": turn 90 degrees to the left (i.e., anti-clockwise direction).
+        "R": turn 90 degrees to the right (i.e., clockwise direction).
+
+    The robot performs the instructions given in order, and repeats them forever.
+    Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
+Time Complexity: O(n), where n is the length of the instructions string
+"""
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
         # initialize the starting direction as north
