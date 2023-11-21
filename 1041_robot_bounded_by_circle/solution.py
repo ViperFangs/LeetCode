1d0c25b946084f59af973b475c3cd3c3c03449d7
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-11-21 02:36:57 -0700

    Add comments to solution.py for Robot Bounded by Circle

--- 1041_robot_bounded_by_circle/solution.py
+++ 1041_robot_bounded_by_circle/solution.py
@@ -1,14 +1,41 @@
 class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
+        # initialize the starting direction as north
         dir_x, dir_y = 0, 1
+        # initialize the starting position as the origin (0, 0)
         x, y = 0, 0
-
+        # loop through the instructions
         for cmd in instructions:
+            # if the current instruction is "G" for go, then move the robot in the direction it's facing
             if cmd == "G":
                 x, y = x + dir_x, y + dir_y
+            # if the current instruction is "L" for left, then change the robot's direction to the left respective of it's current direction
             if cmd == "L":
+                """
+                Use vector math to change the direction to left
+                e.g. if the current direction is (-1, 0) which is facing the left direction,
+                    then taking a left should face the robot in the south direction
+                    dir_x = -1 * dir_y = -1 * 0 => 0
+                    dir_y = dir_x = -1 => -1
+                    the final direction becomes (0, -1) after applying the vector math which is facing the south direction 
+                """
                 dir_x, dir_y = -1 * dir_y, dir_x
+            # if the current instruction is "R" for right, then change the robot's direction to the right respective of it's current direction
             if cmd == "R":
+                """
+                Use vector math to change the direction to left
+                e.g. if the current direction is (-1, 0) which is facing the left direction,
+                    then taking a right should face the robot in the north direction
+                    dir_x = dir_y = 0 => 0
+                    dir_y = -1 * dir_x = -1 * -1 => 1
+                    the final direction becomes (0, 1) after applying the vector math which is facing the north direction 
+                """
                 dir_x, dir_y = dir_y, -1 * dir_x
-    
+        """
+        After applying all the instructions:
+        1. If the robot's current position is the same as the starting position then the robot is in a loop (a circle) -> return True
+        2. If the robot's current position is different than the starting position then we check if there is a change in direction
+            2.1. if there is a change in direction from the initial direction (north) then the robot is in a loop (a circle) -> return True
+            2.2. if there is NO change in the direction then the robot is not in a loop (NOT a circle) -> return False
+        """
         return (x, y) == (0, 0) or (dir_x, dir_y) != (0, 1)
