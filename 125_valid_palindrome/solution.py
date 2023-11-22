da047d1f9ef861dac81c8b9f20d4c683f1b513ba
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-11-22 01:10:09 -0700

    Add comments to the solution for Valid Palindrome

--- 125_valid_palindrome/solution.py
+++ 125_valid_palindrome/solution.py
@@ -1,23 +1,39 @@
+"""
+Author: Aarya
+Description: A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+    Given a string s, return true if it is a palindrome, or false otherwise.
+Time Complexity: O(n), where n is the length of the str string
+"""
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # create a left pointer at the start of the string
         l = 0
+        # create a right pointer at the end of the string
         r = len(s) - 1
-
+        # loop through the string until the left pointer becomes greater than the right pointer 
         while l < r:
+            # if the current character at the left pointer is not an alphanumeric then increment the left pointer
             while l < r and not s[l].isalnum():
                 l += 1
+            # if the current character at the right pointer is not an alphanumeric then decrement the right pointer
             while r > l and not s[r].isalnum():
                 r -= 1
+            # if the character at the left pointer is not the same as the character at the right pointer then return False
             if s[l].lower() != s[r].lower():
                 return False
+            # increment the left pointer and decrement the right pointer
             l, r = l + 1, r - 1
+        # if the code hasn't returned False by now then the string is a palindrome: return True
         return True
-
+    # alternate implmentation for the solution, requires O(n) space
     def isPalindromeP(self, s: str) -> bool:
+        # create an empty string
         seq = ""
-
+        # loop through the parameter str
         for c in s:
+            # if the current character is alphanumeric then append it to the str
             if c.isalnum():
                 seq += c.lower()
+        # Check if the reverse string is the same as the forward string. return True if it's the same, else return false
         return seq == seq[::-1]
         
