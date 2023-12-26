80a679c1d1562ee893c9d092b0f57e402093cc5f
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-12-26 01:01:52 -0700

    Update solution.py
    Write comments for 1929. Concatenation of Array

--- 1929_concatenation_of_array/solution.py
+++ 1929_concatenation_of_array/solution.py
@@ -1,5 +1,16 @@
+""" 
+Author: Aarya
+Description: Given an integer array nums of length n, 
+    you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+    Specifically, ans is the concatenation of two nums arrays.
+    Return the array ans.
+Time Complexity: O(n), where n is the length of the nums list.
+Space Complexity: O(1), the algorithm uses no extra variables
+Logic: The logic here is to loop through each variable and append it to the end.
+"""
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
+        # Loop through the list and add each variable to the end.
         for i in range(len(nums)):
             nums.append(nums[i])
         
