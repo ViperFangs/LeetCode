607a2ae8468d0180b2e5a1064e74c2c4fa5a1b9d
ViperFangs <77452390+ViperFangs@users.noreply.github.com>
2023-12-28 02:42:22 -0700

    Update solution.py
    Write comments for 1472. Design Browser History

--- 1472_design_browser_history/solution.py
+++ 1472_design_browser_history/solution.py
@@ -1,9 +1,24 @@
+""" 
+Author: Aarya
+Description: You have a browser of one tab where you start on the homepage and you can visit another url, get back in the history number of steps or move forward in the history number of steps.
+    Implement the BrowserHistory class:
+        BrowserHistory(string homepage) Initializes the object with the homepage of the browser.
+        void visit(string url) Visits url from the current page. It clears up all the forward history.
+        string back(int steps) Move steps back in history. If you can only return x steps in the history and steps > x, you will return only x steps. 
+            Return the current url after moving back in history at most steps.
+        string forward(int steps) Move steps forward in history. If you can only forward x steps in the history and steps > x, you will forward only x steps. 
+            Return the current url after forwarding in history at most steps.
+Space Complexity: O(n), the algorithm uses a doubly linked list to do the operations
+Logic: The logic here is to use a doubly linked list to store the next and previous browser pages visited.
+    Note: There is another solution that uses a list which is faster for some operations.
+"""
+# BrowserNode stores the url, next and previous values
 class BrowserNode:
     def __init__(self, val = "", next = None, prev = None):
         self.val = val
         self.next = next
         self.prev = prev
-
+# BrowserHistory keeps track of a pointer to navigate through the history
 class BrowserHistory:
     def __init__(self, homepage: str):
         self.current = BrowserNode(homepage)
