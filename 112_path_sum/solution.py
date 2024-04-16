49b5fb7add0d53e078d9e2d71e292063d357bb8a
ViperFangs <amathrej@asu.edu>
2024-04-15 17:24:46 -0700

    Add comments to Path Sum solution

--- 112_path_sum/solution.py
+++ 112_path_sum/solution.py
@@ -7,14 +7,17 @@ class TreeNode:
 
 class Solution:
       def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        # Return False if the current node is None
         if not root:
             return False
-
+        # If the current node is a Leaf Node and if the targetSum is reached then return True
         if not root.left and not root.right and targetSum - root.val == 0:
             return True
+        # Recursively check the left node
         if self.hasPathSum(root.left, targetSum - root.val):
             return True
+        # Recursively check the right node
         if self.hasPathSum(root.right, targetSum - root.val):
             return True
-
+        # If no solution was found then return False
         return False
