bd84a388408ead6dbc6f26480697c6915489b7b2
ViperFangs <amathrej@asu.edu>
2024-04-24 16:48:04 -0700

    Implement Iterative Solution for Searching in a Binary Search Tree

--- 700_search_in_a_binary_search_tree/solution.py
+++ 700_search_in_a_binary_search_tree/solution.py
@@ -9,3 +9,15 @@ class Recursive:
             return self.searchBST(root.right, val)
         else:
             return self.searchBST(root.left, val)
+        
+class Iterative:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        while root:
+            if root.val == val:
+                return root
+            elif root.val < val:
+                root = root.right
+            else:
+                root = root.left
+        
+        return None
