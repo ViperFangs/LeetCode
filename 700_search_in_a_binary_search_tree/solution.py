5648c9e748b7c5e14e3ca7fbdf467affb6c1f2a2
ViperFangs <amathrej@asu.edu>
2024-04-24 16:53:32 -0700

    Add author notes at the top of the solution for searching in a binary search tree

--- 700_search_in_a_binary_search_tree/solution.py
+++ 700_search_in_a_binary_search_tree/solution.py
@@ -1,3 +1,18 @@
+""" 
+Author: Aarya
+Description: You are given the root of a binary search tree (BST) and an integer val.
+  Find the node in the BST that the node's value equals val and return the subtree rooted with that node.
+  If such a node does not exist, return null.
+Time Complexity: O(log(n)), Both the solutions take log(n) time to execute as BST is sorted 
+  and the algorithm gets rid of half the input on each execution.
+Space Complexity: O(1) for Iterative; O(log(n)) for Recursive
+  The iterative solution only uses a few pointers that dont grow with the input.
+  For a recursive function, the space time complexity is equal to the depth of the call tree.
+Logic: 
+  Iterative: Go to the right or left subtree depending on the root's value and target value.
+  Recursive: Recursively call the same method on the right or left subtree depending on the root's value and target value.
+"""
+
 class Recursive:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
         if not root:
