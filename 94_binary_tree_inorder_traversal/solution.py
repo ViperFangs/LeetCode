4a337f5e81dfd5ecd9cd1d4fed14b74c5214f067
ViperFangs <amathrej@asu.edu>
2024-04-28 11:07:32 -0700

    Add author notes at the top of the solution for binary tree inorder traversal

--- 94_binary_tree_inorder_traversal/solution.py
+++ 94_binary_tree_inorder_traversal/solution.py
@@ -1,3 +1,18 @@
+""" 
+Author: Aarya
+Description: Given the root of a binary tree, return the inorder traversal of its nodes' values.
+Time Complexity: O(n), Both the solutions takes n time to execute, 
+  as it has to go through all the nodes in the tree
+Space Complexity: O(n) for both solutions
+  Both the solutions use a list to keep track of all the node values,
+    the list will grow with the size of the input
+Logic: 
+  Iterative: Keep going to the left using a temp pointer, once at the end pop the stack,
+    add the val to result and change temp node to point to the right subtree.  
+  Recursive: Recursively call the same method on the left subtree, then add the current node to the result,
+    then recursively call the method on the right subtree
+"""
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -8,7 +23,6 @@ class TreeNode:
 # This class defines a method `inorderTraversal` that performs a iterative inorder
 # traversal on a binary tree and returns the result as a list of integers.
 class Iterative:
-    # Keep going to the left using a temp pointer, once at the end pop the stack, add the val to result and change temp node to point to the right subtree. 
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         result, stack = [], []
         current_node = root
