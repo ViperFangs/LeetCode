f459f21b33a16e398b72ac6b0ea89460b5d1a70c
ViperFangs <amathrej@asu.edu>
2024-05-02 14:20:09 -0700

    Add author notes to the solution for 39 Combination Sum
    Add Name, Description, Time and Space Complexity

--- 39_combination_sum/solution.py
+++ 39_combination_sum/solution.py
@@ -1,3 +1,18 @@
+"""
+Author: Aarya
+Description: Given an array of distinct integers candidates and a target integer target, 
+  return a list of all unique combinations of candidates where the chosen numbers sum to target. 
+  You may return the combinations in any order.
+  The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+  frequency of at least one of the chosen numbers is different.
+Time Complexity: O(n), where n is the amount of variables in the tree. 
+    Backtracking solutions generally have the time complexity of O(n)
+    This is because in the worst case, the solution will go through all the variables.
+Space Complexity: O(n), where n is the amount of variables in the tree. 
+    Backtracking solutions generally have the space complexity of O(n)
+    This is because in the worst case, the solution will go through all the variables.
+"""
+
 # This class provides a method that finds all unique combinations of candidates
 # that sum up to a target value using backtracking.
 class Solution:
